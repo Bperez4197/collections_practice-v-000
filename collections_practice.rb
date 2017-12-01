63140da85436e5230481ce1e746e6e65cff8e5fc
Bperez4197 <bryceperez31@gmail.com>
2017-12-01 09:50:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -30,5 +30,15 @@ end
 def sum_array(array)
   sum = 0
   array.each {|num| sum += num}
-  sum 
+  sum
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
